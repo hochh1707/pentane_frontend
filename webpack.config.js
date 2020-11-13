285bf72d43f8283ac7483034fa6da2a5b5f07f71
unknown <hochh1707@gmail.com>
2020-11-13 11:19:21 -0600

    heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,21 @@
 
-const path = require('path');
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }]
-  }
-};
+module.exports = (env) =>{
+  const isProduction = env === "production";
+  console.log("env",env);
+  const path = require('path');
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }]
+    }
+  };  
+}
+
